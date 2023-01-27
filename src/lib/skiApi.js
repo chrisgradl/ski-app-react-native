7b4b034607110e5de74bb8dce618993e97a72c57
cgradl <christian.gradl@icloud.com>
2023-01-27 17:01:58 +0100

    added detail screen

--- src/lib/skiApi.js
+++ src/lib/skiApi.js
@@ -1,6 +1,14 @@
-const url = 'https://appfeeds.orf.at/alpine.v2/api/cuprankings/4';
+
 
 export function fetchWorldCupMale() {
+    const url = 'https://appfeeds.orf.at/alpine.v2/api/cuprankings/4';
     return fetch(url)
         .then(res => res.json());
+}
+
+export function fetchPersonDetail(id) {
+    const url = `https://appfeeds.orf.at/alpine.v2/api/person/${id}`
+    return fetch(url)
+        .then(res => res.json());
+
 }
--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -3,25 +3,38 @@ import { View, Text, ScrollView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Avatar, Card, Title } from "react-native-paper";
+import { useQuery } from "react-query";
+import { fetchPersonDetail } from "../lib/skiApi";
 
 export default function DetailScreen({ route }) {
   const { item } = route.params;
 
-  console.log(item);
+  const { PersonId } = item;
 
-  const { Rank, Value, PersonImage, NationImage, NationCC3 } = item;
+  const { data } = useQuery(["person", PersonId], () =>
+    fetchPersonDetail(PersonId)
+  );
+
+  console.log(data);
 
   return (
     <ScrollView contentContainerStyle={{ flex: 1, padding: 16 }}>
       <View style={{ marginBottom: 16, alignItems: "center" }}>
-        <Avatar.Image source={{ uri: PersonImage }} size={200} />
+        <Avatar.Image source={{ uri: data?.PersonImage }} size={200} />
       </View>
       <Card>
         <Card.Content>
-          <Avatar.Image source={{ uri: NationImage }} />
-          <Title>Rang: {Rank}</Title>
-          <Title>Punkte: {Value}</Title>
-          <Title>Nation: {NationCC3}</Title>
+          <Avatar.Image source={{ uri: data?.NationImage }} />
+          <Title>Rang: {data?.WcRank}</Title>
+          <Title>Punkte: {data?.WcPoints}</Title>
+          <Title>Nation: {data?.NationName}</Title>
+          <Title>Equipment: {data?.Equipment}</Title>
+
+          <Title>Alter: {data?.Age}</Title>
+          <Title>Größe: {data?.Height} cm</Title>
+          <Title>Nation: {data?.NationName}</Title>
+          <Title>Residence: {data?.Residence}</Title>
+          <Title>Geboren am: {new Date(data?.BirthDay).toLocaleDateString()}</Title>
         </Card.Content>
       </Card>
     </ScrollView>
